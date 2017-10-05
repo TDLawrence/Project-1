b04b65adfc86de049398364ae2b926754767e008
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-05 09:22:59 -0400

    MultiplicationTable

---
+++ Project 1.1 Tyson Lawrence Period 2/src/TLlib.java
@@ -0,0 +1,15 @@
+
+public class TLlib {
+
+	public static void main(String[] args) {
+      multiplicationTable (3, 30);
+	}
+
+    public static void multiplicationTable (int base, int range)
+    {int n=0;
+      while (n<=range)
+     {System.out.println (base*n);
+      n++;
+     }
+    }
+}
