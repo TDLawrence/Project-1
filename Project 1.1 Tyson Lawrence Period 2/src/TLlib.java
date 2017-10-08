406db282102bb7c3b918b0774906f8da101ed962
Tyson <Tyson@Tyson-PC.fios-router.home>
2017-10-08 17:55:49 -0400

    added powers

--- Project 1.1 Tyson Lawrence Period 2/src/TLlib.java
+++ Project 1.1 Tyson Lawrence Period 2/src/TLlib.java
@@ -2,7 +2,7 @@
 public class TLlib {
 //Tyson Lawrence Period 2 Project 1.1 MethodMadness october 5
 	public static void main(String[] args) {
-      multiplicationTable (3, 30);
+      multiplicationTable (3, 30);Powers (2,17);
 	}
 
     public static void multiplicationTable (int base, int range)
@@ -11,5 +11,10 @@ public class TLlib {
      {System.out.println (base*n);
       n++;
      }
+    }
+    public static void Powers (int base1, int range1)
+    {int n=0;
+     if (n<=range1)
+     {System.out.println (base1^n);}
     }
 }
