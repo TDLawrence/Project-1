7a418bf23dae8abb06b47424a1ce31b924637d20
Tyson <Tyson@Tyson-PC.fios-router.home>
2017-10-08 20:50:26 -0400

    Created Compiler Class.

---
+++ Project 1.1 Tyson Lawrence Period 2/src/CompilerClass.java
@@ -0,0 +1,10 @@
+
+public class CompilerClass {
+
+	public static void main(String[] args) {
+		// Combined Main Method Tyson Lawrence Amy Chen Period 2
+		multiplicationTable(2,20); Powers(2,10); dateStr("11/7/2009");
+
+	}
+
+}
