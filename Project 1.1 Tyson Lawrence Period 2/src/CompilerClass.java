3b2dba8c13d2fb9fb0719f11c0b2c316831997d1
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-10 09:27:50 -0400

    completed

--- Project 1.1 Tyson Lawrence Period 2/src/CompilerClass.java
+++ Project 1.1 Tyson Lawrence Period 2/src/CompilerClass.java
@@ -3,7 +3,8 @@ public class CompilerClass {
 
 	public static void main(String[] args) {
 		// Combined Main Method Tyson Lawrence Amy Chen Period 2
-		TLlib.multiplicationTable(2,20); TLlib.Powers(2,10); TLlib.dateStr("11/7/2009");
+		TLlib.multiplicationTable(3, 30);TLlib.Powers(2, 10);TLlib.dateStr("11/7/09");
+		AClib.isFibonacci(3);AClib.isPalindrome("2332");AClib.sumUpTo(25);
 
 	}
 
